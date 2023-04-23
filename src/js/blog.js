884e651dafaa605ea30df268daf68c9a22cc43b1
michidoma <michidmaap6@gmail.com>
2023-04-23 20:05:53 +0800

    blog, contact paged header, footer nemsen

---
+++ src/js/blog.js
@@ -0,0 +1,18 @@
+setTimeout(() => {
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.menu');
+  
+    console.log("ICON ", menu);
+    menu.onclick = () => {
+        menu.classList.toggle('bx-x');
+        navbar.classList.toggle('open');
+    }
+  }, 100);
+  
+$(window).scroll(function () {
+    if ($(window).scrollTop()) {
+      $("nav").addClass("green");
+    } else {
+      $("nav").removeClass("green");
+    }
+});
