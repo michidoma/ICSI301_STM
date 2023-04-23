881483c904f669b49bc60efd5fd7690d7f01d744
michidoma <michidmaap6@gmail.com>
2023-04-23 18:04:16 +0800

    travel-details page timer, tab-nav busad

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,35 @@
 setTimeout(() => {
+    const cardSlider = document.querySelectorAll(".card-slider-container");
+    console.log(cardSlider);
+    const cardSliderWidth = cardSlider[0].offsetWidth;
+    const cards = document.querySelector("custom-card");
+    const cardWidth = cards.offsetWidth;
+    const cardMarginRight = parseInt(
+      window.getComputedStyle(document.querySelector("custom-card")).marginRight
+    );
+    const cardMarginLeft = parseInt(
+      window.getComputedStyle(document.querySelector("custom-card")).marginLeft
+    );
+    const cardCount = 6;
+    let offset = 0,
+      offset2 = 0;
+    const maxX = -((cardWidth + cardMarginRight) * (cardCount - 4));
+    console.log("HMM ", cardWidth);
+  
+    document.querySelector(".prev-btn").addEventListener("click", () => {
+      if (offset !== 0) {
+        offset += cardWidth + cardMarginRight;
+        cardSlider[0].style.transform = `translateX(${offset}px)`;
+      }
+    });
+  
+    document.querySelector(".next-btn").addEventListener("click", () => {
+      if (offset > maxX) {
+        offset -= cardWidth + cardMarginRight;
+        cardSlider[0].style.transform = `translateX(${offset}px)`;
+      }
+    });
+  
     let menu = document.querySelector('#menu-icon');
     let navbar = document.querySelector('.menu');
   
@@ -7,12 +38,12 @@ setTimeout(() => {
         menu.classList.toggle('bx-x');
         navbar.classList.toggle('open');
     }
-}, 100);
+  }, 100);
   
-$(window).scroll(function () {
+  $(window).scroll(function () {
     if ($(window).scrollTop()) {
       $("nav").addClass("green");
     } else {
       $("nav").removeClass("green");
     }
-});
+  });
