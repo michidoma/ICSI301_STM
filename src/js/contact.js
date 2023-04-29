20e1856118d056db0fb48a48ce8ace9e8c6a5b02
roozu17 <bsarnai068@gmail.com>
2023-04-29 18:53:40 +0800

    Add button animation in contact page

---
+++ src/js/contact.js
@@ -0,0 +1,81 @@
+setTimeout(() => {
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.menu');
+  
+    console.log("ICON ", menu);
+    menu.onclick = () => {
+        menu.classList.toggle('bx-x');
+        navbar.classList.toggle('open');
+    }
+  }, 100);
+  
+$(window).scroll(function () {
+    if ($(window).scrollTop()) {
+      $("nav").addClass("green");
+    } else {
+      $("nav").removeClass("green");
+    }
+});
+
+document.cookie = "cookie_name=cookie_value; SameSite=Strict";
+
+// button animation
+const submitButton = document.getElementById("submitButton");
+
+const inputs = [
+  document.getElementById("name"),
+  document.getElementById("phone"),
+  document.getElementById("email"),
+  document.getElementById("message")
+];
+
+submitButton.addEventListener("click", (event) => {
+  // Prevent the page from refreshing
+  event.preventDefault();
+
+  // Check if all the inputs are filled
+  let isAllFilled = true;
+  for (const input of inputs) {
+    if (input.value.trim() === "") {
+      isAllFilled = false;
+      break;
+    }
+  }
+
+  // If all the inputs are filled, show the animation
+  if (isAllFilled) {
+    // Disable the button to prevent multiple clicks
+    submitButton.disabled = true;
+
+    // Show the loading animation
+    submitButton.innerHTML = "<div class='loader'></div>";
+
+    // Simulate a 2-second loading animation
+    setTimeout(() => {
+      // Change the text to "Successfully sent"
+      submitButton.innerHTML = "Илгээгдлээ";
+      submitButton.style = "background: #186b4e; color: #ffffff; pointer-events: noene;"
+
+      // Enable the button again
+      // submitButton.disabled = false;
+    }, 3000);
+  }
+});
+
+
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault();
+
+//   const formData = {
+//     name: nameInput.value,
+//     phone: phoneInput.value,
+//     email: emailInput.value,
+//     message: messageInput.value
+//   };
+
+//   // setTimeout(() => {
+//   //   submitBtn.classList.remove('loading');
+//   // }, 3000);
+
+//   console.log(formData);
+// });
