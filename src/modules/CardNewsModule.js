adef6c0f8bc668407fae1c84edc14f5d17993d0a
michidoma <michidmaap6@gmail.com>
2023-05-18 13:27:38 +0800

    cardnews style test

--- src/modules/CardNewsModule.js
+++ src/modules/CardNewsModule.js
@@ -9,102 +9,6 @@ class NewsItem {
 
   Render() {
     return `
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">  
-    <style>
-      .blog-card {
-        width: 100%;
-        height: 200px;
-        display: flex;
-        margin: 2em 0;
-        padding: 0 1vw;
-      }
-      .blog-card img {
-          width: 200px;
-          height: 200px;
-          object-fit: cover;
-          border-radius: 15%;
-      }
-      .blog-info {
-          padding: 1em;
-          display: flex;
-          flex-direction: column;
-          justify-content: space-between;
-      }
-      .blog-info h2 {
-          margin: 0;
-          font-family: 'PT Sans';
-      }
-      .blog-info p {
-          height: 78px;
-          text-align: justify;
-          overflow: hidden;
-          text-overflow: ellipsis;
-      }  
-      .blog-info button {
-        border: 0;
-        background-color: transparent;
-        cursor: pointer;
-        padding: 0;
-      }
-      .blog-info button a {
-        text-decoration: none;
-        color: var(--primary-orange);
-        font-family: 'Open Sans', sans-serif;
-        font-weight: 600;
-        display: flex;
-        align-items: center;
-      }
-      .blog-info button i {
-          font-size: 1.5em;
-          margin-left: 8px;
-      }
-      @media only screen and (max-width: 992px) {
-        .blog-info h2{
-          font-size: 1rem;
-        }
-      }
-      @media only screen and (max-width: 835px) {
-        .blog-card img {
-          height: 150px;
-          width: 150px;
-        }
-        .blog-card {
-            height: 150px;
-        }  
-        .blog-info {
-            padding: 0.5em 1em;
-        }
-      }
-      @media only screen and (max-width: 768px) {
-        .blog-card img {
-            height: 200px;
-            width: 200px;
-        }
-        .blog-card {
-            height: 200px;
-            width: 100%;
-        }
-        .blog-info h2 {
-            font-size: 1.5em;
-        }
-      }
-      @media only screen and (max-width: 560px) {
-        .blog-card {
-          margin: 1rem 0;
-          height: 150px;
-        }
-        .blog-card img {
-            height: 150px;
-            width: 150px;
-        }
-        .blog-info h2 {
-            font-size: 1em;
-        }
-        .blog-info p {
-            height: 60px;
-        }
-      }
-    </style>
     <div class="news-card">
     <article class="blog-card">
       <img src="${this.image}" class="blog-img" alt="${this.altText}">
@@ -135,7 +39,7 @@ export default class CardNews {
       .then((result) => {
         result.json().then((jsob) => {
           console.log("RESULT", jsob);
-          const filteredArray = jsob.record.filter(
+          const filteredArray = jsob.news.filter(
             (travelItem) => travelItem.status == "active"
           );
 
@@ -162,7 +66,7 @@ export default class CardNews {
 const gebi = (id) => document.getElementById(id);
 
 const activeTravels = new CardNews(
-  "https://api.jsonbin.io/v3/b/6458fb5a9d312622a35a10d8"
+  "../../db.json"
 );
 activeTravels.download("blog-container");
 // setTimeout(() => {
